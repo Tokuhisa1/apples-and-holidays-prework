2e578806c684f7d482730341cd800aa8a6e7de9e
Winston Tokuhisa <tokuhisawinston@outlook.com>
2018-07-12 16:06:49 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,6 +62,15 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
+  bbq = []
   # Return an array of holiday names (as symbols)
   # where supply lists include the string "BBQ"
+  holiday_hash.each do |season, holiday|
+    holiday_hash[season].each do |day, items|
+      if items.include?("BBQ")
+        bbq << day
+      end
+    end
+  end
+  bbq
 end
